26129a5b35278cc7a7f8a517372cabd6073980f6
ichx <czn16@qq.com>
2020-10-18 19:12:51 +0800

    [*] improve performance

--- eset.go
+++ eset.go
@@ -74,34 +74,30 @@ func(es *ExpirableSet) largerThan(other *ExpirableSet) bool {
 
 func(es *ExpirableSet) Add(elem interface{}) {
 	es.mutex.Lock()
-	defer es.mutex.Unlock()
-
 	es.add(elem, nil)
+	es.mutex.Unlock()
 }
 
 
 func(es *ExpirableSet) AddWithExpire(elem interface{}, expireTime time.Duration) {
 	es.mutex.Lock()
-	defer es.mutex.Unlock()
-
 	es.add(elem, es.buildBase(expireTime))
+	es.mutex.Unlock()
 }
 
 
 func(es *ExpirableSet) Update(oldElem interface{}, newElem interface{}) {
 	es.mutex.Lock()
-	defer es.mutex.Unlock()
-
 	es.elems[newElem] = es.elems[oldElem]
 	delete(es.elems, oldElem)
+	es.mutex.Unlock()
 }
 
 
 func(es *ExpirableSet) Remove(elem interface{}) {
 	es.mutex.Lock()
-	defer es.mutex.Unlock()
-
 	delete(es.elems, elem)
+	es.mutex.Unlock()
 }
 
 
@@ -112,8 +108,6 @@ func(es *ExpirableSet) GetCapacity() int {
 
 func(es *ExpirableSet) GetAll() []interface{} {
 	es.mutex.Lock()
-	defer es.mutex.Unlock()
-
 	var tempSlice []interface{}
 	for elem, base := range es.elems {
 		if base.isExpired() {
@@ -122,49 +116,40 @@ func(es *ExpirableSet) GetAll() []interface{} {
 			tempSlice = append(tempSlice, elem)
 		}
 	}
+
+	es.mutex.Unlock()
 	return tempSlice
 }
 
 
 func(es *ExpirableSet) Contains(elem interface{}) bool {
 	es.mutex.Lock()
-	defer es.mutex.Unlock()
-
 	base, isExist := es.elems[elem]
+	es.mutex.Unlock()
 	return isExist && !base.isExpired()
 }
 
 
 func(es *ExpirableSet) Clear() {
-	es.mutex.Lock()
-	defer es.mutex.Unlock()
-
 	es.init()
 }
 
 
 func(es *ExpirableSet) Union(other *ExpirableSet) *ExpirableSet {
-	es.mutex.Lock()
-	other.mutex.Lock()
-	defer es.mutex.Unlock()
-	defer other.mutex.Unlock()
-
 	lagerEs, smallEs := compareAndGet(es, other)
+	smallEs.mutex.Lock()
 	for elem := range smallEs.elems {
 		if !lagerEs.contains(elem) {
 			lagerEs.elems[elem] = lagerEs.elems[elem]
 		}
 	}
+
+	smallEs.mutex.Unlock()
 	return lagerEs
 }
 
 
 func(es *ExpirableSet) Intersect(other *ExpirableSet) *ExpirableSet {
-	es.mutex.Lock()
-	other.mutex.Lock()
-	defer es.mutex.Unlock()
-	defer other.mutex.Unlock()
-
 	newEs := New()
 	var lagerEs, smallEs *ExpirableSet
 	if es.largerThan(other) {
@@ -175,49 +160,53 @@ func(es *ExpirableSet) Intersect(other *ExpirableSet) *ExpirableSet {
 		smallEs = es
 	}
 
+	smallEs.mutex.Lock()
 	for elem := range smallEs.elems {
 		if lagerEs.contains(elem) {
 			newEs.elems[elem] = smallEs.elems[elem]
 		}
 	}
+
+	smallEs.mutex.Unlock()
 	return newEs
 }
 
 
 func(es *ExpirableSet) Different(other *ExpirableSet) *ExpirableSet {
-	es.mutex.Lock()
-	other.mutex.Lock()
-	defer es.mutex.Unlock()
-	defer other.mutex.Unlock()
-
 	lagerEs, smallEs := compareAndGet(es, other)
+
+	smallEs.mutex.Lock()
 	for elem := range smallEs.elems {
 		if lagerEs.contains(elem) {
 			delete(lagerEs.elems, elem)
 		} else {
-			lagerEs.elems[elem] = lagerEs.elems[elem]
+			lagerEs.elems[elem] = smallEs.elems[elem]
 		}
 	}
+
+	smallEs.mutex.Unlock()
 	return lagerEs
 }
 
 
 func(es *ExpirableSet) Equal(other *ExpirableSet) bool {
-	es.mutex.Lock()
-	other.mutex.Lock()
-	defer es.mutex.Unlock()
-	defer other.mutex.Unlock()
-
 	if len(es.elems) != len(other.elems) {
 		return false
 	}
 
+	es.mutex.Lock()
+	other.mutex.Lock()
+
 	for elem := range other.elems {
 		if !es.contains(elem) {
+			es.mutex.Unlock()
+			other.mutex.Unlock()
 			return false
 		}
 	}
 
+	es.mutex.Unlock()
+	other.mutex.Unlock()
 	return true
 }
 
@@ -232,9 +221,8 @@ func(es *ExpirableSet) Clone() *ExpirableSet {
 
 func(es *ExpirableSet) Size() int {
 	es.mutex.Lock()
-	defer es.mutex.Unlock()
-
 	es.delExpiredElems()
+	es.mutex.Unlock()
 	return len(es.elems)
 }
 
